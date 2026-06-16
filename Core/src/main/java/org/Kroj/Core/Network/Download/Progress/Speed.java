65ffe8e2bd50166942e1f2f7ce7a820d83a19c6d
KarojiHamana <abolfazlaminzadeh1388@gmail.com>
2026-06-16 08:57:06 +0330

    Added Better Progress Tracking

--- Core/src/main/java/org/Kroj/Core/Network/Download/Progress/Speed.java
+++ Core/src/main/java/org/Kroj/Core/Network/Download/Progress/Speed.java
@@ -1,4 +1,41 @@
 package org.Kroj.Core.Network.Download.Progress;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Speed {
+
+    private record Moment(long time, long bytes) { }
+
+    private final Queue<Moment> buffer = new LinkedList<>();
+    private final long bufferLength;
+
+    public Speed(long bufferLength) {
+        this.bufferLength = bufferLength;
+    }
+
+    public synchronized double updateAndGetSpeed(long current) {
+        long now = System.currentTimeMillis();
+        buffer.add(new Moment(now, current));
+
+        while (!buffer.isEmpty() && (now - buffer.peek().time) > bufferLength) {
+            buffer.poll();
+        }
+
+        if (buffer.size() < 2) {
+            return 0.0;
+        }
+
+        Moment oldest = buffer.peek();
+        long elapsed = now-oldest.time;
+        long bytes = current-oldest.bytes;
+
+        if (elapsed == 0) return 0.0;
+
+        return (bytes*1000.0)/elapsed;
+    }
+
+    public synchronized void reset() {
+        buffer.clear();
+    }
 }
