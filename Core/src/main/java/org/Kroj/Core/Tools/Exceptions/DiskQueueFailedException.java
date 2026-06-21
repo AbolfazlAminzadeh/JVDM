0f69907cb277ef909099963e580d8af485eb20bd
KarojiHamana <abolfazlaminzadeh1388@gmail.com>
2026-06-21 10:27:32 +0330

    Added: Better Exceptions for future troubleshoots

---
+++ Core/src/main/java/org/Kroj/Core/Tools/Exceptions/DiskQueueFailedException.java
@@ -0,0 +1,7 @@
+package org.Kroj.Core.Tools.Exceptions;
+
+public class DiskQueueFailedException extends RuntimeException{
+    public DiskQueueFailedException(String message) {
+        super(message);
+    }
+}
---
+++ Core/src/main/java/org/Kroj/Core/Tools/Exceptions/TooMuchRedirections.java
@@ -0,0 +1,7 @@
+package org.Kroj.Core.Tools.Exceptions;
+
+public class TooMuchRedirections extends RuntimeException {
+    public TooMuchRedirections(String message) {
+        super(message);
+    }
+}
