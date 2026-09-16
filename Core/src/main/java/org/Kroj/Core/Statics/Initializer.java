28e69ed3e98f579dee523d9c8dc4667d8b0a483d
KarojiHamana <abolfazlaminzadeh1388@gmail.com>
2026-09-16 20:56:56 +0330

    Improved: Going to be dynamic

--- Core/src/main/java/org/Kroj/Core/Statics/Initializer.java
+++ Core/src/main/java/org/Kroj/Core/Statics/Initializer.java
@@ -2,74 +2,72 @@ package org.Kroj.Core.Statics;
 
 
 import io.netty.util.concurrent.DefaultThreadFactory;
-import org.Kroj.Core.Network.DNS.Resolver;
 
 import java.io.File;
-import java.net.InetSocketAddress;
 import java.util.concurrent.ThreadFactory;
 
-public final class Initializer {
+public class Initializer {
 
     // -=-=-=-=-=-=-=-=-=-=-=-
     //          JVDM
     // -=-=-=-=-=-=-=-=-=-=-=-
 
-    public final static float VERSION = 1.3F;
+    public final static float VERSION = 1.7F;
 
     public final static int CPU_THREADS = Runtime.getRuntime().availableProcessors();
 
     public final static int JVDM_THREADS = CPU_THREADS * 2;
 
-    public final static String DOWNLOAD_FOLDER = System.getProperty("user.home")+ File.separator + "Downloads";
+    public static String DOWNLOAD_FOLDER = System.getProperty("user.home")+ File.separator + "Downloads";
 
     // -=-=-=-=-=-=-=-=-=-=-=-
     //          Core
     // -=-=-=-=-=-=-=-=-=-=-=-
 
-    public final static CharSequence USER_AGENT = "JVDM-".concat(System.getProperty("os.name").toLowerCase()).concat("-V").concat(Float.toString(VERSION));
+    public static CharSequence USER_AGENT = "JVDM-".concat(System.getProperty("os.name").toLowerCase()).concat("-V").concat(Float.toString(VERSION));
 
-    public final static int CONNECTION_TIMEOUT = 10000;
+    public static int CONNECTION_TIMEOUT = 10000;
 
-    public final static int TCP_TIMEOUT = 15000;
+    public static int TCP_TIMEOUT = 15000;
 
-    public final static int RECEIVE_TIMEOUT = 10000;
+    public static int RECEIVE_TIMEOUT = 10000;
 
-    public final static int TCP_IDLE = 60;
+    public static int TCP_IDLE = 60;
 
-    public final static int TCP_INTERVAL = 15;
+    public static int TCP_INTERVAL = 15;
 
-    public final static int TCP_MAX_TRIES = 3;
+    public static int TCP_MAX_TRIES = 3;
 
-    public final static int DOWNLOADER_THREADS = JVDM_THREADS * 3 / 2;
+    public static int DOWNLOADER_CONNECTIONS = JVDM_THREADS * 3 / 2;
 
-    public static final int MAX_RETRIES = 5;
+    public static int MAX_RETRIES = 5;
 
-    public static final int RETRY_DELAY = 1000;
+    public static int RETRY_DELAY = 1000;
 
     // Suggest User H2 Is Always better for download - MUX boosts speed
     public final static boolean H2_PREFER = true;
 
-    public final static int MAX_REDIRECTIONS = 10;
+    public static int MAX_REDIRECTIONS = 10;
 
-    public final static int SPLIT_PART_INTERVAL = 2500;
+    public static int SPLIT_PART_INTERVAL = 2500;
 
-    public final static int SPLIT_PART_MIN_THRESHOLD_BYTE = 1 << 20;
+    public static int SPLIT_PART_MIN_THRESHOLD_BYTE = 1 << 20;
 
-    public final static int RECEIVE_BUFFER_SIZE = 1 << 22;
+    public static int RECEIVE_BUFFER_SIZE = 1 << 22;
 
-    public final static int SEND_BUFFER_SIZE = 1 << 20;
+    public static int SEND_BUFFER_SIZE = 1 << 20;
 
-    public final static int MINIMUM_BUFFER_SIZE = 1 << 16;
+    public static int MINIMUM_BUFFER_SIZE = 1 << 16;
 
-    public final static int INITIAL_BUFFER_SIZE = 1 << 22;
+    public static int INITIAL_BUFFER_SIZE = 1 << 22;
 
-    public final static int MAXIMUM_BUFFER_SIZE = 1 << 24;
+    public static int MAXIMUM_BUFFER_SIZE = 1 << 24;
 
     // -=-=-=-=-=-=-=-=-=-=-=-
     //            UI
     // -=-=-=-=-=-=-=-=-=-=-=-
 
-    public final static int MIN_UI_UPDATE = 10;
+    public static int MIN_UI_UPDATE = 10;
 
     // -=-=-=-=-=-=-=-=-=-=-=-
     //           TLS
@@ -81,7 +79,7 @@ public final class Initializer {
     //        Progress
     // -=-=-=-=-=-=-=-=-=-=-=-
 
-    public final static int PROGRESS_INTERVAL = 100;
+    public static int PROGRESS_INTERVAL = 100;
 
 
     // -=-=-=-=-=-=-=-=-=-=-=-
@@ -137,6 +135,6 @@ public final class Initializer {
     //          Info
     // -=-=-=-=-=-=-=-=-=-=-=-
 
-    public static final int totalThreads = JVDM_THREADS+schedulerThread;
+    public static int totalThreads = JVDM_THREADS+schedulerThread;
 
 }
